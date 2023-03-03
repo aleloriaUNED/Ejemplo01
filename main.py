450a11ff8a2d070d8c7cc58b27d3cfcff90ebca7
aleloria <alloria@uned.ac.cr>
2023-03-02 18:25:23 -0600

    Se implementan un cuarto saludo.

--- main.py
+++ main.py
@@ -14,4 +14,5 @@ if __name__ == '__main__':
     print_hi('PyCharm')
     print_hi('Alejandro')
     print_hi('TODOS')
+    print_hi('Cuarto saludo')
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
