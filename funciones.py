811751e54427f4016703f80a0d561ba5cc907c9e
aleloria <alloria@uned.ac.cr>
2023-03-02 19:23:02 -0600

    Cambio de ejemplos

---
+++ funciones.py
@@ -0,0 +1,7 @@
+def imprimir (variable):
+    print('El valor digitado es: ')
+    print(variable)
+
+def captura():
+    var = input("Introduzca un valor: ")
+    return var
--- main.py
+++ main.py
@@ -1,18 +1,9 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import funciones,
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+a = funciones.captura()
+
+funciones.imprimir(a)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-    print_hi('Alejandro')
-    print_hi('TODOS')
-    print_hi('Cuarto saludo')
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
